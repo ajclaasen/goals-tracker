23f5936a9d320916492e4b70ac73e217adaa0952
Raphaël Claasen <raphael.claasen@gmail.com>
2021-01-28 13:24:16 +0100

    Change session number to integer type

--- db/migrate/20210128122151_create_sessions.rb
+++ db/migrate/20210128122151_create_sessions.rb
@@ -1,7 +1,7 @@
 class CreateSessions < ActiveRecord::Migration[6.1]
   def change
     create_table :sessions do |t|
-      t.number :number
+      t.integer :number
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_133150) do
+ActiveRecord::Schema.define(version: 2021_01_28_122151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2021_01_26_133150) do
   create_table "goals", force: :cascade do |t|
     t.string "text"
     t.integer "reward"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
