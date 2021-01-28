6b11f0e0f7e189a56b0f7ed903f4a05e613ef5f3
Raphaël Claasen <raphael.claasen@gmail.com>
2021-01-28 14:29:09 +0100

    Require presence of goal text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_123308) do
+ActiveRecord::Schema.define(version: 2021_01_28_132534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_28_123308) do
   end
 
   create_table "goals", force: :cascade do |t|
-    t.string "text"
+    t.string "text", null: false
     t.integer "reward"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
